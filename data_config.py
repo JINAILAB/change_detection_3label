a21186abbc58a6ca41aef4c189a137efaecb361b
kkksoinsane <99486264+kkksoinsane@users.noreply.github.com>
2022-11-12 22:31:07 +0900

    Update data_config.py

--- data_config.py
+++ data_config.py
@@ -7,7 +7,7 @@ class DataConfig:
         self.data_name = data_name
         if data_name == 'LEVIR':
             self.label_transform = "norm"
-            self.root_dir = '/home/jhsong/ChangeFormer/LEVIR-CD256/'
+            self.root_dir = '/raid/parksonice/'
         elif data_name == 'DSIFN':
             self.label_transform = "norm"
             self.root_dir = '/media/lidan/ssd2/CDData/DSIFN_256/'
