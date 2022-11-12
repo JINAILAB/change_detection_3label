bb98f45bbbb85598638949b0393e0db8f4552215
kkksoinsane <99486264+kkksoinsane@users.noreply.github.com>
2022-11-12 22:58:07 +0900

    Update eval_ChangeFormer_LEVIR.sh

--- scripts/eval_ChangeFormer_LEVIR.sh
+++ scripts/eval_ChangeFormer_LEVIR.sh
@@ -5,10 +5,10 @@ gpus=0,1
 data_name=LEVIR
 net_G=ChangeFormerV6 #This is the best version
 split=test
-vis_root=/home/jhsong/ChangeFormer/vis
+vis_root=/raid/parksonice/change_detection_3label/vis/
 project_name=CD_ChangeFormerV6_LEVIR_b16_lr0.0001_adamw_train_test_200_linear_ce_multi_train_True_multi_infer_False_shuffle_AB_False_embed_dim_256
-checkpoints_root=/home/jhsong/ChangeFormer/checkpoints/
-checkpoint_name=best_ckpt.pt
+checkpoints_root=/raid/parksonice/
+checkpoint_name=last_ckpt.pt
 img_size=256
 embed_dim=256 #Make sure to change the embedding dim (best and default = 256)
 
