866104f72547d47409c264e41a3ccbb30227feef
kkksoinsane <99486264+kkksoinsane@users.noreply.github.com>
2022-11-14 01:41:39 +0900

    Update run_ChangeFormer_LEVIR.sh

--- scripts/run_ChangeFormer_LEVIR.sh
+++ scripts/run_ChangeFormer_LEVIR.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #GPUs
-gpus=0,1
+gpus=0,1,2
 
 #Set paths
 checkpoint_root=/raid/parksonice/change_detection_3label/checkpoints
@@ -10,9 +10,9 @@ data_name=LEVIR
 
 
 img_size=256    
-batch_size=16   
-lr=0.0001         
-max_epochs=200
+batch_size=32
+lr=0.0002       
+max_epochs=300
 embed_dim=256
 
 net_G=ChangeFormerV6        #ChangeFormerV6 is the finalized verion
@@ -35,4 +35,4 @@ split_val=val      #test
 project_name=CD_${net_G}_${data_name}_b${batch_size}_lr${lr}_${optimizer}_${split}_${split_val}_${max_epochs}_${lr_policy}_${loss}_multi_train_${multi_scale_train}_multi_infer_${multi_scale_infer}_shuffle_AB_${shuffle_AB}_embed_dim_${embed_dim}
 
 
-CUDA_VISIBLE_DEVICES='0,1' python3 main_cd.py --img_size ${img_size} --loss ${loss} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --optimizer ${optimizer} --split ${split} --split_val ${split_val} --net_G ${net_G} --multi_scale_train ${multi_scale_train} --multi_scale_infer ${multi_scale_infer} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --shuffle_AB ${shuffle_AB} --data_name ${data_name}  --lr ${lr} --embed_dim ${embed_dim} --n_class ${n_class} --pretrain ${pretrain}
+CUDA_VISIBLE_DEVICES='0,1,2' python3 main_cd.py --img_size ${img_size} --loss ${loss} --checkpoint_root ${checkpoint_root} --vis_root ${vis_root} --lr_policy ${lr_policy} --optimizer ${optimizer} --split ${split} --split_val ${split_val} --net_G ${net_G} --multi_scale_train ${multi_scale_train} --multi_scale_infer ${multi_scale_infer} --gpu_ids ${gpus} --max_epochs ${max_epochs} --project_name ${project_name} --batch_size ${batch_size} --shuffle_AB ${shuffle_AB} --data_name ${data_name}  --lr ${lr} --embed_dim ${embed_dim} --n_class ${n_class} --pretrain ${pretrain}
