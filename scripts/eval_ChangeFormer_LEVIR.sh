e79bc0864f60fa9367b4bcc4df361482c0ce2695
kkksoinsane <99486264+kkksoinsane@users.noreply.github.com>
2022-11-17 11:15:53 +0900

    Update eval_ChangeFormer_LEVIR.sh

--- scripts/eval_ChangeFormer_LEVIR.sh
+++ scripts/eval_ChangeFormer_LEVIR.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gpus=0,1
+gpus=0,1,2
 
 data_name=LEVIR
 net_G=ChangeFormerV6 #This is the best version
@@ -13,6 +13,6 @@ img_size=256
 embed_dim=256 #Make sure to change the embedding dim (best and default = 256)
 
 
-CUDA_VISIBLE_DEVICES=0,1 python3 eval_cd.py --split ${split} --net_G ${net_G} --embed_dim ${embed_dim} --img_size ${img_size} --vis_root ${vis_root} --checkpoints_root ${checkpoints_root} --checkpoint_name ${checkpoint_name} --gpu_ids ${gpus} --project_name ${project_name} --n_class 4 --data_name ${data_name}
+CUDA_VISIBLE_DEVICES=0,1,2 python3 eval_cd.py --split ${split} --net_G ${net_G} --embed_dim ${embed_dim} --img_size ${img_size} --vis_root ${vis_root} --checkpoints_root ${checkpoints_root} --checkpoint_name ${checkpoint_name} --gpu_ids ${gpus} --project_name ${project_name} --n_class 4 --data_name ${data_name}
 
 
